bf9ac5fd8da5954fedad9e7e26cfb3876664f789
claudiosmweb <contato@claudiosmweb.com>
2013-09-06 19:46:56 -0300

    fixed fallback notice

--- woocommerce-boleto.php
+++ woocommerce-boleto.php
@@ -18,7 +18,7 @@ define( 'WC_BOLETO_URL', plugin_dir_url( __FILE__ ) );
  * WooCommerce fallback notice.
  */
 function wcboleto_woocommerce_fallback_notice() {
-    echo '<div class="error"><p>' . sprintf( __( 'WooCommerce Boleto Gateway depends on the last version of %s to work!' , 'wcboleto' ), '<a href="http://wordpress.org/extend/plugins/woocommerce/">WooCommerce</a>' ) . '</p></div>';
+    echo '<div class="error"><p>' . sprintf( __( 'WooCommerce Boleto Gateway depends on the last version of %s to work!' , 'wcboleto' ), '<a href="http://wordpress.org/extend/plugins/woocommerce/">' . __( 'WooCommerce', 'wcboleto' ) . '</a>' ) . '</p></div>';
 }
 
 /**
