242137428bef94a17a925d3f512e7235db59801b
claudiosmweb <contato@claudiosmweb.com>
2013-12-14 01:56:54 -0200

    added new bradesco option

--- includes/class-wc-boleto-gateway.php
+++ includes/class-wc-boleto-gateway.php
@@ -378,7 +378,8 @@ class WC_Boleto_Gateway extends WC_Payment_Gateway {
 						'options' => array(
 							'03' => '03',
 							'06' => '06',
-							'09' => '09'
+							'09' => '09',
+							'25' => '25'
 						)
 					)
 				);
