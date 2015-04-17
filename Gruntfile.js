e85becf09ed08bab1b371c7b6e93bd85c25fcc68
Claudio Sanches <contato@claudiosmweb.com>
2015-04-17 18:43:55 -0300

    Updated the grunt scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,98 +1,159 @@
-module.exports = function(grunt) {
-"use strict";
+/* jshint node:true */
+module.exports = function( grunt ) {
+'use strict';
 
-    grunt.initConfig({
+	grunt.initConfig({
 
-        // gets the package vars
-        pkg: grunt.file.readJSON("package.json"),
-        svn_settings: {
-            path: "../../../../wp_plugins/<%= pkg.name %>",
-            tag: "<%= svn_settings.path %>/tags/<%= pkg.version %>",
-            trunk: "<%= svn_settings.path %>/trunk",
-            exclude: [
-                ".editorconfig",
-                ".git/",
-                ".gitignore",
-                "node_modules/",
-                "Gruntfile.js",
-                "README.md",
-                "package.json",
-                "*.zip"
-            ]
-        },
+		// Setting folder templates
+		dirs: {
+			css:    'assets/css',
+			fonts:  'assets/fonts',
+			images: 'assets/images',
+			js:     'assets/js'
+		},
 
-        // image optimization
-        imagemin: {
-            dist: {
-                options: {
-                    optimizationLevel: 7,
-                    progressive: true
-                },
-                files: [{
-                    expand: true,
-                    cwd: "./",
-                    src: ["screenshot-*.png"],
-                    dest: "./"
-                }]
-            }
-        },
+		// gets the package vars
+		pkg: grunt.file.readJSON( 'package.json' ),
+		svn_settings: {
+			path: '../../../../wp_plugins/<%= pkg.name %>',
+			tag: '<%= svn_settings.path %>/tags/<%= pkg.version %>',
+			trunk: '<%= svn_settings.path %>/trunk',
+			exclude: [
+				'.git/',
+				'.tx/',
+				'.editorconfig',
+				'.gitignore',
+				'.jshintrc',
+				'node_modules/',
+				'Gruntfile.js',
+				'README.md',
+				'package.json',
+				'*.zip'
+			]
+		},
 
-        // rsync commands used to take the files to svn repository
-        rsync: {
-            options: {
-                args: ["--verbose"],
-                exclude: "<%= svn_settings.exclude %>",
-                recursive: true
-            },
-            tag: {
-                options: {
-                    src: "./",
-                    dest: "<%= svn_settings.tag %>"
-                }
-            },
-            trunk: {
-                options: {
-                src: "./",
-                dest: "<%= svn_settings.trunk %>"
-                }
-            }
-        },
+		// Javascript linting with jshint
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			all: [
+				'Gruntfile.js',
+				'<%= dirs.js %>/*/*.js',
+				'!<%= dirs.js %>/*/*.min.js'
+			]
+		},
 
-        // shell command to commit the new version of the plugin
-        shell: {
-            svn_add: {
-                command: 'svn add --force * --auto-props --parents --depth infinity -q',
-                options: {
-                    stdout: true,
-                    stderr: true,
-                    execOptions: {
-                        cwd: "<%= svn_settings.path %>"
-                    }
-                }
-            },
-            svn_commit: {
-                command: "svn commit -m 'updated the plugin version to <%= pkg.version %>'",
-                options: {
-                    stdout: true,
-                    stderr: true,
-                    execOptions: {
-                        cwd: "<%= svn_settings.path %>"
-                    }
-                }
-            }
-        }
-    });
+		// Minify .js files.
+		uglify: {
+			options: {
+				preserveComments: 'some'
+			},
+			dist: {
+				files: [{
+					expand: true,
+					cwd: '<%= dirs.js %>/',
+					src: [
+						'*.js',
+						'!*.min.js'
+					],
+					dest: '<%= dirs.js %>/',
+					ext: '.min.js'
+				}]
+			}
+		},
 
-    // load tasks
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.loadNpmTasks("grunt-rsync");
-    grunt.loadNpmTasks("grunt-shell");
+		// Watch changes for assets
+		watch: {
+			js: {
+				files: [
+					'<%= dirs.js %>/*js',
+					'!<%= dirs.js %>/*.min.js'
+				],
+				tasks: ['jshint', 'uglify']
+			}
+		},
 
-    // default task
-    grunt.registerTask("default", [
-        "rsync:tag",
-        "rsync:trunk",
-        "shell:svn_add",
-        "shell:svn_commit"
-    ]);
+		// Rsync commands used to take the files to svn repository
+		rsync: {
+			options: {
+				args: ['--verbose'],
+				exclude: '<%= svn_settings.exclude %>',
+				syncDest: true,
+				recursive: true
+			},
+			tag: {
+				options: {
+					src: './',
+					dest: '<%= svn_settings.tag %>'
+				}
+			},
+			trunk: {
+				options: {
+				src: './',
+				dest: '<%= svn_settings.trunk %>'
+				}
+			}
+		},
+
+		// Shell command to commit the new version of the plugin
+		shell: {
+			// Remove delete files.
+			svn_remove: {
+				command: 'svn st | grep \'^!\' | awk \'{print $2}\' | xargs svn --force delete',
+				options: {
+					stdout: true,
+					stderr: true,
+					execOptions: {
+						cwd: '<%= svn_settings.path %>'
+					}
+				}
+			},
+			// Add new files.
+			svn_add: {
+				command: 'svn add --force * --auto-props --parents --depth infinity -q',
+				options: {
+					stdout: true,
+					stderr: true,
+					execOptions: {
+						cwd: '<%= svn_settings.path %>'
+					}
+				}
+			},
+			// Commit the changes.
+			svn_commit: {
+				command: 'svn commit -m "updated the plugin version to <%= pkg.version %>"',
+				options: {
+					stdout: true,
+					stderr: true,
+					execOptions: {
+						cwd: '<%= svn_settings.path %>'
+					}
+				}
+			}
+		}
+	});
+
+	// Load tasks
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+	grunt.loadNpmTasks( 'grunt-rsync' );
+	grunt.loadNpmTasks( 'grunt-shell' );
+
+	// Register tasks
+	grunt.registerTask( 'default', [
+		'jshint',
+		'uglify'
+	]);
+
+	// Deploy task
+	grunt.registerTask( 'deploy', [
+		'default',
+		'rsync:tag',
+		'rsync:trunk',
+		'shell:svn_remove',
+		'shell:svn_add',
+		'shell:svn_commit'
+	] );
 };
