106a24a87ec5d5d4c9ab641ef9c01e5551c38173
Claudio Sanches <contato@claudiosmweb.com>
2015-04-17 20:03:00 -0300

    Moved the banks directory to includes

--- banks/cef_sigcb/layout.php
+++
@@ -1,4 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly.
-
-include plugin_dir_path( dirname( dirname( __FILE__ ) ) ) . 'banks/cef/layout.php';
---
+++ includes/banks/cef_sigcb/layout.php
@@ -0,0 +1,6 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+include plugin_dir_path( dirname( dirname( dirname( __FILE__ ) ) ) ) . 'includes/banks/cef/layout.php';
