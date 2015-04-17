94550d38e51583c467dcfa71ec91418e3eae22f8
Claudio Sanches <contato@claudiosmweb.com>
2015-04-17 19:48:34 -0300

    Added help us message

--- includes/views/html-admin-page.php
+++ includes/views/html-admin-page.php
@@ -6,6 +6,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+$reviews_url = 'https://wordpress.org/support/view/plugin-reviews/woocommerce-boleto?filter=5#postform';
+
 ?>
 
 <h3><?php echo $this->method_title; ?></h3>
@@ -19,6 +21,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 <?php echo wpautop( $this->method_description ); ?>
+
+<?php if ( apply_filters( 'woocommerce_boleto_help_message', true ) ) : ?>
+	<div class="updated woocommerce-message">
+		<p><?php printf( __( 'Help us keep the %s plugin free making a %s or rate %s on %s. Thank you in advance!', 'woocommerce-boleto' ), '<strong>' . __( 'WooCommerce Boleto', 'woocommerce-boleto' ) . '</strong>', '<a href="http://claudiosmweb.com/doacoes/">' . __( 'donation', 'woocommerce-boleto' ) . '</a>', '<a href="' . $reviews_url . '" target="_blank">&#9733;&#9733;&#9733;&#9733;&#9733;</a>', '<a href="' . $reviews_url . '" target="_blank">' . __( 'WordPress.org', 'woocommerce-boleto' ) . '</a>' ); ?></p>
+	</div>
+<?php endif; ?>
 
 <table class="form-table">
 	<?php $this->generate_settings_html(); ?>
